44038bce266207659b955a84dc420ccb81aa35fa
mahaa4 <you@example.com>
2021-06-23 16:35:21 -0700

    pushing in a new lab

---
+++ lab2.py
@@ -0,0 +1,25 @@
+"""print('''
+Hello World!!!!
+'''("""
+
+# step 1 
+from typing import Type
+
+
+name = 'LeBron' # string
+age = 35 # integer 
+salary = 37.44 # float
+plays_basketball = '23' # string
+
+# step 2 
+car_name = 'BMW'
+x = 50 
+my_first_name = 'Mahaalakshumi'
+
+print(type(name))
+print(type(age)) 
+print(type(salary)) 
+print(type(plays_basketball)) 
+print(type(car_name)) 
+print(type(x)) 
+print(type(my_first_name))
