8af2b8e53edd9ff12356098a06a7a0116cc9d66b
mahaa4 <you@example.com>
2021-06-29 14:25:36 -0700

    pushing in lab4 into github

---
+++ dictionarypractice.py
@@ -0,0 +1,47 @@
+# step 2
+Menu = {"chicken":1.59, "beef":1.99, "cheese":1.00, "milk":2.50} 
+print(Menu)
+# step 3 
+BTS_age = {"RM": 26, "Jin": 29, "Jungkook": 24}
+RM = BTS_age["RM"]
+Jin = BTS_age["Jin"]
+
+"RM", "Jin"
+
+def Total_age(mem1,mem2):
+    mem1 = BTS_age [mem1]
+    mem2 = BTS_age [mem2]
+    return mem1 + mem2
+
+# print("Total age of RM and Jin is:") 
+# print(Total_age("RM", "Jin")) 
+
+# Step 4
+RM = BTS_age["RM"]
+Jin = BTS_age["Jin"]
+
+"RM", "Jin"
+
+def Sub_age(mem1,mem2):
+    mem1 = BTS_age [mem1]
+    mem2 = BTS_age [mem2]
+    return mem1 - mem2
+
+# print("Difference in age of RM and Jin is:") 
+# print(Sub_age("RM", "Jin")) 
+
+# Step 5 
+RM = BTS_age["RM"]
+Jin = BTS_age["Jin"]
+
+"RM", "Jin"
+
+def Multi_age(mem1,mem2):
+    mem1 = BTS_age [mem1]
+    mem2 = BTS_age [mem2]
+    return mem1 * mem2 
+
+print(" RM multiplied by Jin age is:") 
+print(Multi_age("RM", "Jin")) 
+
+
---
+++ lab4.py
@@ -0,0 +1,9 @@
+def rect_area(width, height):
+    area = width * height
+    return area
+print("the area of 3 x 5 rectangle is: ")
+print(rect_area(3, 5)) 
+
+print("the area of 5 x 5 rectangle is: ")
+print(rect_area(5, 5)) 
+
